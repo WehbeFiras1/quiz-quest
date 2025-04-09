95d552c3ee86fd3f9bb1ccc9ee70f784ebe73053
Vu Anh Minh <steppsftezzellft780@hotmail.com>
2025-04-09 12:13:06 +0700

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app">
+    <h1>Quiz Quest</h1>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+.app {
+  text-align: center;
+  font-family: Arial, sans-serif;
+}
+</style>
+
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Quiz from '../views/Quiz.vue';
+import Leaderboard from '../views/Leaderboard.vue';
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/quiz/:category', component: Quiz },
+  { path: '/leaderboard', component: Leaderboard },
+];
+
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+});
